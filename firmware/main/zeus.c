10dc70b1ab0ec15e38abfd1981b329846e129ec4
Nicklas Frahm <nicklas.frahm@gmail.com>
2022-02-27 11:17:17 +0000

    feat(network): set up ethernet connectivity

--- firmware/main/zeus.c
+++ firmware/main/zeus.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
-#include "esp_log.h"
 #include "esp_event.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "net.h"
+
+#define FIRMWARE_URL                                               \
+  "https://github.com/nicklasfrahm/zeus/releases/latest/download/" \
+  "zeus-esp32.bin"
 
 void app_main(void) {
   // Create the default event loop that is running in background.
-  // ESP_ERROR_CHECK(esp_event_loop_create_default());
-  
-  while (1) {
-    printf("Hello World!\n");
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  // Configure and start the ethernet IP stack.
+  ESP_ERROR_CHECK(net_eth_configure_and_start());
 
-    vTaskDelay(pdMS_TO_TICKS(1000));
+  // TODO: Set up OTA update thread.
+  printf("Hello World!\n");
+
+  while (1) {
+    // Do nothing without entirely blocking all other threads.
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
