1d7cd85148980e36faf9d694bfde00f1e673fb39
Nicklas Frahm <nicklas.frahm@gmail.com>
2022-03-03 21:08:45 +0000

    fix(lib): make danger of memory leaks more clear
    Signed-off-by: Nicklas Frahm <nicklas.frahm@gmail.com>

--- firmware/main/git.h
+++ firmware/main/git.h
@@ -17,13 +17,12 @@ const char* git_url(void);
 const char* git_support_message(void);
 
 /**
- * Generate a download URL for a file of a given release version. Make sure to
- * free the memory after usage to prevent memory leaks.
+ * Generate a download URL for a file of a given release version.
  *
  * @param[in] version Git tag of the version or "latest".
  * @param[in] file Name of the file to be downloaded.
  *
- * @return A heap-allocated string.
+ * @return A heap-allocated string. WATCH OUT FOR MEMORY LEAKS!
  */
 const char* git_release_download_url(const* version, const* file);
 
